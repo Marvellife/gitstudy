c3f1d9e9c8cd3dc0ecc7fe8efcde09fa01b051f6
Marvellife <1184122931@qq.com>
2022-03-28 01:45:24 +0800

    33333

--- src/main/java/com/example/gitlearn/Hello.java
+++ src/main/java/com/example/gitlearn/Hello.java
@@ -8,6 +8,6 @@ package com.example.gitlearn;
  */
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("111111111");
+        System.out.println("2222222");
     }
 }
