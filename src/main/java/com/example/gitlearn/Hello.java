157f8bb52f36559ded3a0545f69804faca9829e5
Marvellife <1184122931@qq.com>
2022-03-28 01:37:12 +0800

    111111

---
+++ src/main/java/com/example/gitlearn/Hello.java
@@ -0,0 +1,13 @@
+package com.example.gitlearn;
+
+/**
+ * 类功能描述
+ *
+ * @Author Administrator
+ * @Date 2022/3/28 1:36
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("111111111");
+    }
+}
